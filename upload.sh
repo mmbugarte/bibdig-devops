0a607063cb43d1b8d46ad83cd37a38623b27ee51
Nicolás Passerini <npasserini@gmail.com>
2024-03-23 15:10:38 -0300

    Primera versión

---
+++ upload.sh
@@ -0,0 +1,34 @@
+#!/bin/zsh
+echo Uploading ...
+
+ls
+server_url=ec2-34-209-244-214.us-west-2.compute.amazonaws.com
+
+rm -f upload.zip
+zip upload.zip -r db_data 
+zip -u upload.zip devops/compose.yml 
+zip -u upload.zip devops/deploy.sh 
+
+scp -i ~/.ssh/docker-uqbar.pem upload.zip ubuntu@$server_url:.
+
+rm -f upload.zip
+
+deploy=$(cat << 'EOF'
+echo Preparing deploy
+
+if [ ! -d /var/mmbu ]; then
+    sudo mkdir -p /var/mmbu
+    sudo chown ubuntu:ubuntu /var/mmbu``
+fi
+
+cd /var/mmbu
+unzip -uo ~/upload.zip
+
+sudo chmod +x ./deploy.sh
+./deploy.sh
+EOF
+)
+
+echo Upload completed, executing remote deploy.
+ssh -i ~/.ssh/docker-uqbar.pem ubuntu@$server_url "$deploy"
+
